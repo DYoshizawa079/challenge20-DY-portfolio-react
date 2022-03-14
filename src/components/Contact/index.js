359815917575d804675284146e9ff0459c7a75c1
DYoshizawa079 <daniely079@gmail.com>
2022-03-13 21:39:23 -0400

    Final changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,67 @@
 import React, { useState } from 'react';
 
 function Contact() {
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    // Set the input fields to blank.
+    const [fieldsState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = fieldsState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        setFormState({...fieldsState, [e.target.name]: e.target.value})
+
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Sorry. Your email is invalid');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`Sorry. Your ${e.target.name} is required`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        
+        if (!errorMessage) {
+            setFormState({ ...fieldsState, [e.target.name]: e.target.value });
+        }
+    }    
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
         <section id="contact">
             <h2 class="left-content">Contact</h2>
             <div class="right-content">
                 <h3>Contact me!</h3>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
-                        <label>Name</label>
-                        <input type="text" name="name" />
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                     </div>
                     <div>
-                        <label>Email Address</label>
-                        <input type="email" name="email" />
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                     </div>
                     <div>
-                        <label>Message</label>
-                        <textarea></textarea>
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
                     </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="errormessage">{errorMessage}</p>
+                        </div>
+                    )}
                     <button type="submit">Submit</button>
                 </form>
                 <h3>Daniel Yoshizawa</h3>
